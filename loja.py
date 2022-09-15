cd5dde8a1bafc9b04569be14fa87cbe8b2027473
Guilherme Calça <guilhermecalca@hotmail.com>
2022-09-15 16:31:27 -0300

    Minimamente funcional

--- loja.py
+++ loja.py
@@ -1,15 +1,15 @@
+import random
 import paho.mqtt.client as paho
 
-class CD:
-    def __init__(self):
+class Loja:
+    def __init__(self, id):
         self.client = paho.Client()
         self.pid = [0] * 210  #[0] = A(100 unidades) [1] = B(60 unidades) [2] = C(20 unidades)...
-        self.fabricas = [] #3 produtos cada sendo A, B, C
-        self.lojas = []   #20 lojas com todos os 210 produtos
-        self.msg = [' '] * 2
+        self.msg = ' '
+        self.id_loja = id
 
         self.fill_products()
-        self.sub_and_connect()
+        self.connect()
 
     #Inicializa o centro de distribuição cheio
     def fill_products(self):
@@ -22,20 +22,8 @@ class CD:
                 self.pid[i] = 20
 
     #Conecta no broker e inscreve nos canais das 20 lojas
-    def sub_and_connect(self):
+    def connect(self):
         self.client.connect('broker.mqttdashboard.com', 1883)
-
-        for i in range(20):
-            self.client.subscribe(f'loja/[{i}]', qos=1)
-
-    #Função que subtrai do CD a qunatidade a ser enviada para loja
-    def restock_store(self, pid):
-        if (pid % 3 == 0):
-            self.pid[pid] -= 100
-        elif (pid % 3 == 1):
-            self.pid[pid] -= 60
-        else:
-            self.pid[pid] -= 20
 
     #Retorna o sinal dependendo da quantidade de produtos disponiveis
     def control_products(self, pid):
@@ -62,7 +50,6 @@ class CD:
 
     #Faz a requisição de um produto para a fabrica
     def refill(self, pid):
-        id_fab = int(pid / 3)
         if (pid % 3 == 0):
             quant = 100
         elif (pid % 3 == 1):
@@ -70,28 +57,23 @@ class CD:
         else:
             quant = 20
 
-        self.client.publish(f'fabrica/[{id_fab}]', pid)
+        self.client.publish(f'loja/[{self.id_loja}]', pid)
         self.pid[pid] += quant
 
-    #Função de recebimento de mensagem mqtt
-    def on_message(self, client, userdata, msg):
-            print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-            self.msg[0] = msg.topic.split('/')[1]
-            self.msg[1] = msg.payload
 
-            loja = self.msg[0]
-            pid = int(self.msg[1])
+    #Loop
+    def loop(self):
+        produto = random.randint(0, 209)
+        quant = random. randint(1, 19)
 
-            print(f'Loja {loja} teve produto {pid} reestocado')
+        print(f'{quant} unidades adquirida do produto {produto} na loja {self.id_loja}')
+        self.pid[produto] -= quant
 
-            self.restock_store(pid)
-            sinal = self.control_products(pid)
-            if (sinal == 'red'):
-                self.refill(pid)
-
-    #Ouve mensagens dos canais inscritos
-    def listen(self):
-            self.client.on_message = self.on_message
-            self.client.loop_forever()
+        sinal = self.control_products(produto)
+        print(f'Sinal produto {produto}: {sinal}')
+        if (sinal == 'red'):
+            print(f'Loja {self.id_loja} requisitando reabastecimento de produto {produto}')
+            self.refill(produto)
+    
             
         
---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+import threading
+from time import sleep
+from loja import Loja
+from CD import CD
+
+lojas = [Loja(i) for i in range(20)]
+
+run = 1
+cd = CD()
+
+threading1 = threading.Thread(target=cd.listen)
+threading1.daemon = True
+threading1.start()
+
+while run:
+    for loja in lojas:
+        yn = random.randint(0,1)
+        sleeptime = random.randint(1,20) / 10
+        if (yn):
+            loja.loop()
+            sleep(sleeptime)
